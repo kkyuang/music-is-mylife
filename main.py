e7c72b4d4d037f492ea336af4dbff70eca32bec6
kkyuang <bluejoy1004@gmail.com>
2024-09-19 10:21:54 +0900

    .

--- main.py
+++ main.py
@@ -4,4 +4,11 @@ import pandas as pd
 
 
 #엑셀 파일 읽어오기
-pd.read_excel('download_links.xlsx')
+df = pd.read_excel('download_links.xlsx')
+
+# 각 행을 딕셔너리로 변환한 리스트로 변환
+records_list = df.to_dict('records')
+
+# 변환된 리스트 출력 (확인용)
+for record in records_list:
+    print(record)
